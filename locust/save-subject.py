3afa63b87dd73ba27a0be2041f1f7ba82b228134
Marcia <tejedamarcia@gmail.com>
2017-05-15 21:47:38 -0300

    Adding example tests with Header and Json content.

---
+++ locust/save-subject.py
@@ -0,0 +1,49 @@
+from locust import HttpLocust, TaskSet, task
+
+
+
+def save_subj(l):
+        materia = {
+            'name': 'Sistemas Distribuidos',
+            'divisions': [
+                {
+                    'comision': 'C1',
+                    'weekdays': [
+                        'Lunes de 18 a 22'
+                    ],
+                    'quota': 35
+                }
+            ],
+            'group': 'basic',
+            'school_year': '201301'
+        }
+        response = l.client.post(
+                        "director/subject",
+                        data=None,
+                        json=materia,
+                        headers={
+                            "X-App-Name":"heroku",
+                            "X-Secure-Key" : "p81lS7JOYsovb41zV41492q6AtKTHfey",
+                            "Content-Type":"application/json",
+                            "X-Director-Token":"estaesmiclave"
+                        },
+                        )
+
+        print "Preview; Response status code:", response.status_code
+
+
+
+class UserTasks(TaskSet):
+    # one can specify tasks like this
+    tasks = [save_subj]
+
+
+
+class WebsiteUser(HttpLocust):
+    """
+    Locust user class that does requests to the locust web server running on localhost
+    """
+    host = "http://127.0.0.1:8089"
+    min_wait = 2000
+    max_wait = 5000
+    task_set = UserTasks
