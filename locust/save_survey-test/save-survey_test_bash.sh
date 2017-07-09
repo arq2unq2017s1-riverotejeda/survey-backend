2840e4e1e2445772b90af8bd81de85fc03fb0c6d
marcia-t <tejedamarcia@gmail.com>
2017-07-09 19:50:38 -0300

    deleting mongodb before start test

--- locust/save_survey-test/save-survey_test_bash.sh
+++ locust/save_survey-test/save-survey_test_bash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+mongo 172.18.0.2:27017/unq --eval "db.dropDatabase()"
 echo "======= Initialize data ========"
 python initialdata_save-survey.py
 echo "======= Finishing initialize data ======"
 echo "======= Starting locust test ======="
-locust --host=http://localhost:9090 -f save-survey_test.py
+locust --host=http://localhost:80 -f save-survey_test.py
 echo "======= Finishing locust test ======"
